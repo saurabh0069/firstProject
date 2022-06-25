3a805b5401673f565cd5e0a805c0d88592d9eb20
Saurabhkumar0069 <104457320+Saurabhkumar0069@users.noreply.github.com>
2022-06-25 11:25:26 +0530

    Implementation of Swagger2

--- management/src/main/java/com/student/management/ManagementApplication.java
+++ management/src/main/java/com/student/management/ManagementApplication.java
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
 @SpringBootApplication
 public class ManagementApplication {
 
@@ -15,6 +21,16 @@ public class ManagementApplication {
 	@Bean
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
+	}
+	
+	
+	@Bean
+	public Docket managementApi() {
+		
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.student.management.controller"))
+				.build();
 	}
 
 }
